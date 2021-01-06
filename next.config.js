0e13f91f1b5268cb4694ae78610d354be5ebc787
ke <wannngkee@gmail.com>
2021-01-06 21:09:14 +0800

    add cdn

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
-const debug = process.env.NODE_ENV !== "production";
+const isProd = process.env.NODE_ENV === "production";
+
+module.exports = {
+  // Use the CDN in production and localhost for development.
+  assetPrefix: isProd
+    ? "https://cdn.statically.io/gh/wannngkee/wannngkee.github.io/gh-pages/"
+    : "",
+};
 
 module.exports = {
   assetPrefix: !debug ? "/my-blog" : "",
